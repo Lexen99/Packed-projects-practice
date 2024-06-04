a4b885d201279d066c92de645ef8964ae6f9cf01
Oliver Lexenberg <o.lexenberg@hotmail.com>
2024-06-04 21:17:23 +0200

    Done with the accordian, you can selected to be able to open multiple question answers with a button click, else you can just open one at the time.

---
+++ src/Components/Accordian/Accordian.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./Accordian.css";
+import data from "../data";
+
+const Accordian = () => {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multipleSelected, setMultipleSelected] = useState([]);
+
+  const handleSingleSelection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  };
+
+  const handleMultiSelection = (getCurrentId) => {
+    let cpyMultiple = [...multipleSelected];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultipleSelected(cpyMultiple);
+  };
+
+  console.log(selected, multipleSelected);
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ||
+              multipleSelected.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Accordian;
