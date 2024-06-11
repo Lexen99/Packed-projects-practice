f6a7447a4c00689e31b5bac17e001290cb393264
Oliver Lexenberg <o.lexenberg@hotmail.com>
2024-06-11 19:55:11 +0200

    created an image slider with a image api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import './App.css'
-import Accordian from './Components/Accordian/Accordian';
-import ColorGenerater from './Components/ColorGenerater/ColorGenerator';
-import StarRating from './Components/StarRating/StarRating';
+import "./App.css";
+import Accordian from "./Components/Accordian/Accordian";
+import ColorGenerater from "./Components/ColorGenerater/ColorGenerator";
+import StarRating from "./Components/StarRating/StarRating";
+import ImageSlider from "./Components/ImageSlider/ImageSlider";
 
 function App() {
-
   return (
-    <div className='app'>
+    <div className="app">
       {/* Accordian component */}
       <Accordian />
       {/* Random color component */}
       <ColorGenerater />
       {/* Star Rating component */}
-      <StarRating noOfStars={10}/>
+      <StarRating noOfStars={10} />
+      {/* ImageSlider Component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"10"}
+        page={"1"}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
